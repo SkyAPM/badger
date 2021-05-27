91c9454a7ecc116932ca57017e819edaa010f4cd
Gao Hongtao <hanahmily@gmail.com>
2021-05-28 05:56:42 +0800

    Add GetAll method to TSet in order to retrieve all versions from a key
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>

--- tset.go
+++ tset.go
@@ -1,6 +1,7 @@
 package badger
 
 import (
+	"bytes"
 	"fmt"
 	"math"
 
@@ -13,34 +14,40 @@ var ErrTSetInvalidTS = errors.New("Timestamp should be greater than 0")
 
 type ExtractFunc func(raw []byte, ts uint64) ([]byte, error)
 
-// TSet is a time-series set, which leverage internal version to store timestamp. The effect-side is you never append a
-// 8-bit value to the key as the version.
-// Once MemTables are flushed, the table.ReduceFunc is invoked to reduce/merge values with identical keys. In contrast,
-// the ExtractFunc helps Get method to extract reduced values from the "vault" created by table.ReduceFunc
+type SplitFunc func(raw []byte) ([][]byte, error)
+
+// TSet is a time-series set, which leverages internal version to store the timestamp. The effect-side is there's no
+// chance to append a 8-bit value to the key as the version.
+// Once MemTables are flushed, the table.ReduceFunc is invoked to reduce/merge values which have identical keys. In contrast,
+// the ExtractFunc helps Get method to extract reduced values from the "vault" created by table.ReduceFunc.
+// TSet also provide GetAll which needs a SplitFunc to retrieve all values in the same key.
 type TSet struct {
 	db          *DB
 	extractFunc ExtractFunc
+	splitFunc   SplitFunc
 }
 
-func NewTSet(db *DB, reduceFunc table.ReduceFunc, extractFunc ExtractFunc) *TSet {
+func NewTSet(db *DB, reduceFunc table.ReduceFunc, extractFunc ExtractFunc, splitFunc SplitFunc) *TSet {
 	db.reduceFunc = reduceFunc
 	return &TSet{
 		db:          db,
 		extractFunc: extractFunc,
+		splitFunc:   splitFunc,
 	}
 }
 
-func (s *TSet) Put(key, val []byte, ts uint64, f func(error)) error {
-	if ts < 1 {
-		return ErrTSetInvalidTS
-	}
-	entry := &Entry{
-		Key:   y.KeyWithTs(key, ts),
-		Value: val,
-	}
-	req, err := s.db.sendToWriteCh([]*Entry{entry})
+func (s *TSet) Put(key, val []byte, ts uint64) error {
+	req, err := s.write(key, val, ts)
 	if err != nil {
-		return fmt.Errorf("failed to send entry to write channel: %v", err)
+		return err
+	}
+	return req.Wait()
+}
+
+func (s *TSet) PutAsync(key, val []byte, ts uint64, f func(error)) error {
+	req, err := s.write(key, val, ts)
+	if err != nil {
+		return err
 	}
 	if f == nil {
 		return nil
@@ -50,6 +57,21 @@ func (s *TSet) Put(key, val []byte, ts uint64, f func(error)) error {
 		f(err)
 	}()
 	return nil
+}
+
+func (s *TSet) write(key, val []byte, ts uint64) (*request, error) {
+	if ts < 1 {
+		return nil, ErrTSetInvalidTS
+	}
+	entry := &Entry{
+		Key:   y.KeyWithTs(key, ts),
+		Value: val,
+	}
+	req, err := s.db.sendToWriteCh([]*Entry{entry})
+	if err != nil {
+		return nil, fmt.Errorf("failed to send entry to write channel: %v", err)
+	}
+	return req, nil
 }
 
 func (s *TSet) Get(key []byte, ts uint64) (val []byte, err error) {
@@ -78,4 +100,37 @@ func (s *TSet) Get(key []byte, ts uint64) (val []byte, err error) {
 		return nil, fmt.Errorf("faliled to get val from leved files: %v", errLC)
 	}
 	return s.extractFunc(vs.Value, ts)
+}
+
+func (s *TSet) GetAll(key []byte) (val [][]byte, err error) {
+	db := s.db
+	if db.IsClosed() {
+		return nil, ErrDBClosed
+	}
+	val = s.seekMemTables(key)
+	if val != nil && len(val) > 0 {
+		return val, nil
+	}
+	vs, errLC := s.db.lc.get(y.KeyWithTs(key, math.MaxUint64), y.ValueStruct{}, 0)
+	if errLC != nil {
+		return nil, fmt.Errorf("faliled to get val from leved files: %v", errLC)
+	}
+	return s.splitFunc(vs.Value)
+}
+
+func (s *TSet) seekMemTables(prefix []byte) [][]byte {
+	tables, decr := s.db.getMemTables()
+	defer decr()
+	s.db.vlog.incrIteratorCount()
+	var iters []y.Iterator
+	for i := 0; i < len(tables); i++ {
+		iters = append(iters, tables[i].sl.NewUniIterator(false))
+	}
+	it := table.NewMergeIterator(iters, false)
+	defer it.Close()
+	r := make([][]byte, 0)
+	for it.Seek(y.KeyWithTs(prefix, math.MaxUint64)); it.Valid() && bytes.HasPrefix(it.Key(), prefix); it.Next() {
+		r = append(r, it.Value().Value)
+	}
+	return r
 }
--- tset_test.go
+++ tset_test.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"reflect"
-	"sync"
 	"testing"
 
 	"github.com/dgraph-io/badger/v3/y"
@@ -39,6 +37,11 @@ func TestNewTSet(t *testing.T) {
 				{[]byte("k2"), 1, []byte{12}},
 				{[]byte("k3"), 2, []byte{72}},
 				{[]byte("k3"), 1, []byte{83}},
+				{[]byte("k3"), 3, []byte{95}},
+				{[]byte("k5"), 2, []byte{44}},
+				{[]byte("k5"), 1, []byte{66}},
+				{[]byte("k4"), 1, []byte{72}},
+				{[]byte("k4"), 2, []byte{53}},
 			},
 		},
 		{
@@ -51,7 +54,7 @@ func TestNewTSet(t *testing.T) {
 		{
 			name: "invalid value",
 			args: []arg{
-				{[]byte("k1"), math.MaxUint64, errVal},
+				{[]byte("k1"), 1, errVal},
 			},
 			wantVLGetErr: true,
 		},
@@ -72,35 +75,38 @@ func TestNewTSet(t *testing.T) {
 		}
 		return []byte{raw[len(raw)-int(ts)]}, nil
 	}
+	splitFunc := func(raw []byte) ([][]byte, error) {
+		if raw == nil || len(raw) < 1 {
+			return nil, fmt.Errorf("can not find data")
+		}
+		if bytes.Equal(raw, errVal) {
+			return nil, fmt.Errorf("invalid value")
+		}
+		bb := make([][]byte, len(raw))
+		for i := range raw {
+			bb[i] = []byte{raw[i]}
+		}
+		return bb, nil
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var dir string
 			runTSetTest(t, "", func(t *testing.T, db *DB) {
-				s := NewTSet(db, reduceFunc, extractFunc)
+				s := NewTSet(db, reduceFunc, extractFunc, splitFunc)
 				dir = db.opt.Dir
 
-				var wg sync.WaitGroup
 				for _, arg := range tt.args {
-					if !tt.wantPutErr {
-						wg.Add(1)
-					}
-					if err := s.Put(arg.key, arg.val, arg.ts, func(err error) {
-						wg.Done()
-						if (err != nil) != tt.wantPutErr {
-							t.Errorf("Put() error = %v, wantPutErr %v", err, tt.wantPutErr)
-						}
-					}); (err != nil) != tt.wantPutErr {
+					if err := s.Put(arg.key, arg.val, arg.ts); (err != nil) != tt.wantPutErr {
 						t.Errorf("Put() error = %v, wantPutErr %v", err, tt.wantPutErr)
 					}
 				}
-				wg.Wait()
 				if !tt.wantVLGetErr {
 					verifyGet(t, tt, s, tt.wantMTGetErr)
 				}
 			})
 			defer removeDir(dir)
 			runTSetTest(t, dir, func(t *testing.T, db *DB) {
-				verifyGet(t, tt, NewTSet(db, reduceFunc, extractFunc), tt.wantVLGetErr)
+				verifyGet(t, tt, NewTSet(db, reduceFunc, extractFunc, splitFunc), tt.wantVLGetErr)
 			})
 		})
 	}
@@ -110,14 +116,36 @@ func verifyGet(t *testing.T, tt test, s *TSet, wantErr bool) {
 	if tt.wantPutErr {
 		return
 	}
+	keyMerge := make(map[string][][]byte, 0)
 	for _, arg := range tt.args {
 		gotVal, err := s.Get(arg.key, arg.ts)
 		if (err != nil) != wantErr {
 			t.Errorf("Get() error = %v, wantGetErr %v", err, wantErr)
 			return
 		}
+
 		if !wantErr && !reflect.DeepEqual(gotVal, arg.val) {
 			t.Errorf("Get() gotVal = %v, want %v", gotVal, arg.val)
+		}
+		if _, ok := keyMerge[string(arg.key)]; !ok {
+			keyMerge[string(arg.key)] = make([][]byte, 3)
+		}
+		keyMerge[string(arg.key)][3-arg.ts] = gotVal
+	}
+	for key, vals := range keyMerge {
+		flattenVals := make([][]byte, 0)
+		for _, v := range vals {
+			if len(v) > 0 {
+				flattenVals = append(flattenVals, v)
+			}
+		}
+		gotVals, err := s.GetAll([]byte(key))
+		if (err != nil) != wantErr {
+			t.Errorf("GetAll() error = %v, wantGetErr %v", err, wantErr)
+			return
+		}
+		if !wantErr && !reflect.DeepEqual(gotVals, flattenVals) {
+			t.Errorf("GetAll() gotVal = %v, want %v", gotVals, flattenVals)
 		}
 	}
 }
